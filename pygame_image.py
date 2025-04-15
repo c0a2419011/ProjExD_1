f250330eeb968d5f623c7afa80673cef5dbe0cf9
c0a2419011 <c0a2419011@edu.teu.ac.jp>
2025-04-15 15:39:38 +0900

    練習2と4　こうかとん描画

--- pygame_image.py
+++ pygame_image.py
@@ -11,13 +11,14 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png") #練習1
+    kk_img = pg.transform.flip(kk_img,True,False)#左右だけ反転させる。
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img,[300,200])
+        screen.blit(kk_img,[300,200])#練習4
         pg.display.update()
         tmr += 1        
         clock.tick(10)
