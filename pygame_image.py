c78d4d52311b45b960f872cb8a879510083ce2ca
c0a2419011 <c0a2419011@edu.teu.ac.jp>
2025-04-15 17:17:12 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -16,25 +16,26 @@ def main():
     kk_rct = kk_img.get_rect() #練習10-1こうかとんrectの抽出
     kk_rct.center = 300,200 #練習10-2 
     tmr = 0
+    move = kk_rct.move_ip
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()#練習10-3
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))#練習10-4
+            move((0,-1))#練習10-4
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))#練習10-4
+            move((0,1))#練習10-4
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))#練習10-4
+            move((-1,0))#練習10-4
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2,0))#練習10-4 この場合x座標y座標逆になっている。+と-逆
+            move((2,0))#練習10-4 この場合x座標y座標逆になっている。+と-逆
             
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])#練習6
         screen.blit(bg_img2, [-x+1600, 0])#練習7
         screen.blit(bg_img, [-x+3200, 0])#練習9
         screen.blit(kk_img,kk_rct)#練習4/練習10-3
-        kk_rct.move_ip((-1,0))
+        move((-1,0))
         pg.display.update()
         tmr += 1        
         clock.tick(200) #練習5
